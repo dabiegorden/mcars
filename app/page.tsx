ec562b80d3c04af7b1658315134fae94e0767695
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-09-15 14:46:52 -0700

    comit
    creating hero page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { Hero } from '@/components';
+
+export default function page() {
+  return (
+    <main className="overflow-hidden">
+        <Hero />
+    </main>
+  )
+}
